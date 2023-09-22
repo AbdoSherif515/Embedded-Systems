ad3f7a25639e14d6d29b48c05fd81efe933779ae
AbdoSherif515 <145264588+AbdoSherif515@users.noreply.github.com>
2023-09-22 17:04:29 +0300

    Update GPIO_interface.h

--- ARM/Drivers/MCAL/2-GPIO/GPIO_interface.h
+++ ARM/Drivers/MCAL/2-GPIO/GPIO_interface.h
@@ -18,6 +18,26 @@ u8   MGPIO_u8GetPinValue (u8 Copy_u8PortId,u8 Copy_u8PinId);
 /* Ports */
 #define  PORTA   0
 
+#define PIN_0_ID 0
+#define PIN_1_ID 1
+#define PIN_2_ID 2
+#define PIN_3_ID 3
+#define PIN_4_ID 4
+#define PIN_5_ID 5
+#define PIN_6_ID 6
+#define PIN_7_ID 7
+#define PIN_8_ID 8
+#define PIN_9_ID 9
+#define PIN_10_ID 10
+#define PIN_11_ID 11
+#define PIN_12_ID 12
+#define PIN_13_ID 13
+#define PIN_14_ID 14
+#define PIN_15_ID 15
+
+
+#define HIGH_PIN 1
+#define LOW_PIN  0
 
 /* Direction */
 #define  GPIOA_INPUT            0
